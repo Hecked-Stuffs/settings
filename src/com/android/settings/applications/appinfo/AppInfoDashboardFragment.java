NusantaraProject-ROM/android_packages_apps_Settings

AppInfo: Fix crash of settings if there is no play store installed

src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -407,8 +407,10 @@ public class AppInfoDashboardFragment extends DashboardFragment
         }
         // Utils.isSystemPackage doesn't include all aosp built apps, like Contacts etc. Add them
         // and grab the Google Play Store itself (com.android.vending) in the process
-        menu.findItem(PLAY_STORE).setVisible(!Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
+        if (com.android.internal.util.nad.NadUtils.isPackageInstalled(getContext(),"com.android.vending")) {
+            menu.findItem(PLAY_STORE).setVisible(!Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
                 && !isAospOrStore(mAppEntry.info.packageName));
+        }
     }
 
     @Override

Hecked-Stuffs/settings

src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -434,8 +434,10 @@ public class AppInfoDashboardFragment extends DashboardFragment
         }
         // Utils.isSystemPackage doesn't include all aosp built apps, like Contacts etc. Add them
         // and grab the Google Play Store itself (com.android.vending) in the process
-        menu.findItem(PLAY_STORE).setVisible(!Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
+        if (com.android.internal.util.derp.derpUtils.isPackageInstalled(getContext(),"com.android.vending")) {
+            menu.findItem(PLAY_STORE).setVisible(!Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
                 && !isAospOrStore(mAppEntry.info.packageName));
+        }
     }
 
     private static void showLockScreen(Context context, Runnable successRunnable) {